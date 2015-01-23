8db6fa1d216e09b53599ee9bbf3b53cfe7a3ed52
Yu-han Lin <guesslin1986@gmail.com>
2015-01-23 15:09:58 +0800

    Add: how to debug decorated function

---
+++ function_decorators11.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Debugging decorated functions
+use func.__name__ to check
+'''
+
+
+def tags(tag_name):
+    def tags_decorator(func):
+        def func_wrapper(*args, **kwargs):
+            return '<{0}>{1}</{0}>'.format(tag_name, func(*args, **kwargs))
+        return func_wrapper
+    return tags_decorator
+
+
+class Person(object):
+    def __init__(self):
+        self.name = "Guesslin"
+        self.last = "Lin"
+
+    @tags('div')
+    @tags('p')
+    @tags('strong')
+    def get_fullname(self):
+        return self.name + ' ' + self.last
+
+my_person = Person()
+
+print my_person.get_fullname()
+print my_person.get_fullname.__name__
